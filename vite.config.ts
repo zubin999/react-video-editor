f716ba3a86ba451f3898bc66a48725d8bdffdc2a
李祖彬 <lizubin@macdeMacBook-Pro.local>
2025-06-06 17:31:41 +0800

    Remove commented out proxy configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,16 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // server: {
-  //   proxy: {
-  //     '/appapi': {
-  //       target: 'http://app.v4.xinmem.com/',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path,
-  //       secure: false,
-  //       timeout: 30000,
-  //     }
-  //   } 
-  // },
   base: process.env.NODE_ENV === "production" ? "/quick-edit/" : "/",
 });
